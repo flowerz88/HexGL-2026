33ac54f59a2c53e729fe7e4f26954c966f21e261
Butterfly88 <191980868+flowerz88@users.noreply.github.com>
2026-07-24 23:26:26 +0200

    Enhance isTouchDevice to include pointer media query

--- bkcore.coffee/Utils.js
+++ bkcore.coffee/Utils.js
@@ -228,8 +228,13 @@
      */
 
     Utils.isTouchDevice = function() {
-      return ('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
-    };
+  return (
+    ('ontouchstart' in window) ||
+    (navigator.maxTouchPoints > 0) ||
+    (navigator.msMaxTouchPoints > 0) ||
+    window.matchMedia && window.matchMedia('(pointer: coarse)').matches
+  );
+};
 
     return Utils;
 
